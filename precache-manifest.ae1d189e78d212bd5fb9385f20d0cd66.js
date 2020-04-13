e0e73a0304521ab87ffc8d88d824e8fabc7a3bff
YunJehyeok <dbswpgur2@naver.com>
2020-04-14 02:32:44 +0900

    Updates

---
+++ precache-manifest.ae1d189e78d212bd5fb9385f20d0cd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cbdcb4de95396acff3ecb1f43cc0aa",
+    "url": "/movie_react/index.html"
+  },
+  {
+    "revision": "8c1df0331b5c5611c7d6",
+    "url": "/movie_react/static/css/main.ed7120f0.chunk.css"
+  },
+  {
+    "revision": "e4fa36edabbbed92a845",
+    "url": "/movie_react/static/js/2.292e9e51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_react/static/js/2.292e9e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1df0331b5c5611c7d6",
+    "url": "/movie_react/static/js/main.a7ff1d30.chunk.js"
+  },
+  {
+    "revision": "898cf4b35e67902b8e95",
+    "url": "/movie_react/static/js/runtime-main.373cc0a4.js"
+  }
+]);
